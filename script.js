44260a0350211b6503507fb8267f480e7661a67e
Samuel Stenfelt <samuel.stenfelt@utb.karlshamn.se>
2022-03-31 11:02:04 +0200

    Update script.js
    initiated creation of addition function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Working");
+//alert("Working");
+
+//mathematical operators
+
+var currentVal = 0;
+
+function sum()
+{
+    //while (true)
+    {
+        var b = parseInt(prompt());
+        var result = currentVal + b;
+        currentVal = result;
+    }    
+}
+
+sum();
+console.log(currentVal);
